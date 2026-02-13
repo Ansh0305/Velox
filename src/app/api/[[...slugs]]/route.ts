f5ad978f403d6be29f0a743dbf685c02f2d3cde4
Ansh0305 <saiansh2016@gmail.com>
2026-02-13 16:03:37 +0530

    Added auth-protected message endpoint with Zod schema

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,6 +1,8 @@
 import { redis } from "@/lib/redis";
 import { Elysia, t } from "elysia";
 import { nanoid } from "nanoid";
+import { authMiddleware } from "./auth";
+import { z } from "zod";
 
 const ROOM_TTL_SECONDS = 60 * 10;
 
@@ -10,14 +12,34 @@ const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
   await redis.hset(`meta:${roomId}`, {
     connected: [],
     createAt: Date.now(),
-  })
+  });
 
-  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
 
-  return {roomId}
+  return { roomId };
 });
 
-const app = new Elysia({ prefix: "/api" }).use(rooms);
+const messages = new Elysia({ prefix: "/messages" }).use(authMiddleware).post(
+  "/",
+  async ({ body, auth }) => {
+    const { sender, text } = body;
+    const { roomId } = auth;
+    const roomExits = await redis.exists(`meta:${roomId}`);
+
+    if (!roomExits) {
+      throw new Error("Room does not exist!");
+    }
+  },
+  {
+    query: z.object({ roomId: z.string() }),
+    body: z.object({
+      sender: z.string().max(100),
+      text: z.string().max(1000),
+    }),
+  },
+);
+
+const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages);
 
 export const GET = app.fetch;
 export const POST = app.fetch;
