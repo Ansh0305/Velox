a28162173571fe659293b9ca2a1f860da87077a4
Ansh0305 <saiansh2016@gmail.com>
2026-02-13 13:56:09 +0530

    Implemented room creation with Redis and expiration(10min)

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,7 +1,20 @@
+import { redis } from "@/lib/redis";
 import { Elysia, t } from "elysia";
+import { nanoid } from "nanoid";
 
-const rooms = new Elysia({ prefix: "/room" }).post("/create", () => {
-  console.log("Create a new Room!");
+const ROOM_TTL_SECONDS = 60 * 10;
+
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createAt: Date.now(),
+  })
+
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS)
+
+  return {roomId}
 });
 
 const app = new Elysia({ prefix: "/api" }).use(rooms);
