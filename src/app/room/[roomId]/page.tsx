335a9bce67868cab741dd2bdffc1bdcb5db3b2eb
Ansh0305 <saiansh2016@gmail.com>
2026-02-13 14:35:05 +0530

    Added self-destruct countdown UI

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -7,16 +7,24 @@ const Page = () => {
   const params = useParams();
   const roomId = params.roomId as string;
 
-  
   // Copy button
-  const [copyStatus, setcopyStatus] = useState("COPY")
+  const [copyStatus, setcopyStatus] = useState("COPY");
   const copyLink = () => {
-    const url = window.location.href
-    navigator.clipboard.writeText(url)
-    setcopyStatus("COPIED!")
+    const url = window.location.href;
+    navigator.clipboard.writeText(url);
+    setcopyStatus("COPIED!");
     setTimeout(() => {
-        setcopyStatus("COPY")
+      setcopyStatus("COPY");
     }, 2000);
+  };
+
+  // Remaininig Time
+  const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+
+  function formatTimeRemaining(seconds: number) {
+    const mins = Math.floor(seconds / 60)
+    const secs = seconds % 60
+    return `${mins}:${secs.toString().padStart(2, "0")}`
   }
   return (
     <main className="flex flex-col h-screen max-h-screen overflow-hidden">
@@ -25,11 +33,23 @@ const Page = () => {
           <div className="flex flex-col">
             <span className="text-xs text-zinc-500 uppercase">Room ID</span>
             <div className="flex items-center gap-2">
-                <span className="font-bold text-green-500">{roomId}</span>
-                <button onClick={copyLink} className="text-[10px] bg-zinc-800 hover:bg-zinc-700 px-2 py-0.5 rounded text-zinc-400 hover:text-zinc-200 transition-colors">
-                    {copyStatus}
-                </button>
+              <span className="font-bold text-green-500">{roomId}</span>
+              <button
+                onClick={copyLink}
+                className="text-[10px] bg-zinc-800 hover:bg-zinc-700 px-2 py-0.5 rounded text-zinc-400 hover:text-zinc-200 transition-colors"
+              >
+                {copyStatus}
+              </button>
             </div>
+          </div>
+          <div className="h-8 w-px bg-zinc-800" />
+          <div className="flex flex-col">
+            <span className="text-xs text-zinc-500 uppercase">
+              Self-Destruct
+            </span>
+            <span
+              className={`text-sm font-bold flex items-center gap-2 ${timeRemaining !== null && timeRemaining < 60 ? "text-red-500" : "text-amber-500"}`}
+            >{timeRemaining !== null ? formatTimeRemaining(timeRemaining) : "--:--"}</span>
           </div>
         </div>
       </header>
