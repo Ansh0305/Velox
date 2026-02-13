7912ac7c2a9aaab143aaf622f72ca1d2dc2b8567
Ansh0305 <saiansh2016@gmail.com>
2026-02-13 14:56:05 +0530

    Added message input and chat layout UI

--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -22,9 +22,9 @@ const Page = () => {
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
 
   function formatTimeRemaining(seconds: number) {
-    const mins = Math.floor(seconds / 60)
-    const secs = seconds % 60
-    return `${mins}:${secs.toString().padStart(2, "0")}`
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    return `${mins}:${secs.toString().padStart(2, "0")}`;
   }
   return (
     <main className="flex flex-col h-screen max-h-screen overflow-hidden">
@@ -49,7 +49,11 @@ const Page = () => {
             </span>
             <span
               className={`text-sm font-bold flex items-center gap-2 ${timeRemaining !== null && timeRemaining < 60 ? "text-red-500" : "text-amber-500"}`}
-            >{timeRemaining !== null ? formatTimeRemaining(timeRemaining) : "--:--"}</span>
+            >
+              {timeRemaining !== null
+                ? formatTimeRemaining(timeRemaining)
+                : "--:--"}
+            </span>
           </div>
         </div>
 
@@ -57,8 +61,25 @@ const Page = () => {
           DESTROY NOW
           <span className="group-hover:animate-pulse">💥</span>
         </button>
-
       </header>
+      <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"></div>
+
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/50">
+        <div className="flex gap-4">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-500 animate-pulse">
+              {">"}
+            </span>
+            <input
+              autoFocus
+              type="text"
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-8 text-sm"
+            />
+          </div>
+
+          <button className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer">SEND</button>
+        </div>
+      </div>
     </main>
   );
 };
