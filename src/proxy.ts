119ba09136d95f12c9b4a0d59cf5fa96791de9f9
Ansh0305 <saiansh2016@gmail.com>
2026-02-13 15:36:37 +0530

    Implemented room join middleware with Redis and token auth

---
+++ src/proxy.ts
@@ -0,0 +1,59 @@
+// Middleware
+
+import { NextRequest, NextResponse } from "next/server";
+import { redis } from "./lib/redis";
+import { nanoid } from "nanoid";
+
+export const proxy = async (req: NextRequest) => {
+  // Checking if the user is allowed to join the room
+  // If they are => Join the room
+  // If they are not => Send them back to the lobby
+
+  const pathname = req.nextUrl.pathname;
+
+  const roomMatch = pathname.match(/^\/room\/([^/]+)$/);
+  if (!roomMatch) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  const roomId = roomMatch[1];
+
+  const meta = await redis.hgetall<{ connected: string[]; createdAt: number }>(
+    `meta:${roomId}`,
+  );
+
+  if (!meta)
+    return NextResponse.redirect(new URL("/?error=room-not-found", req.url));
+
+  // Existing user
+  const existingToken = req.cookies.get("x-auth-token")?.value
+
+  // Existing user rejoin the room(Refresh)
+  if (existingToken && meta.connected.includes(existingToken)) {
+    return NextResponse.next()
+  }
+  // User not allowed to join 
+  if (meta.connected.length >= 2) {
+    return NextResponse.redirect(new URL("/?error=room-full", req.url))
+  }
+
+  const response = NextResponse.next();
+
+  const token = nanoid();
+  response.cookies.set("x-auth-token", token, {
+    path: "/",
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+  });
+
+  await redis.hset(`meta:${roomId}`, {
+    connected: [...meta.connected, token],
+  });
+
+  return response;
+};
+
+export const config = {
+  matcher: "/room/:path",
+};
