01d1335fee0b5f48820aef410160ec6b30c4da6d
Ansh0305 <saiansh2016@gmail.com>
2026-02-14 15:25:04 +0530

    Production error hardcoded localhost

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,5 +1,5 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-export const client =treaty<App>('localhost:3000').api
+export const client = treaty<App>(process.env.NEXT_PUBLIC_APP_URL ?? 'localhost:3000').api
 
